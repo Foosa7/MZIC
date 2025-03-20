a313f86097ce96714a11252a0e7afd2b7f876eab
Andrea Cataldo <andreacl@kth.se>
2025-03-20 20:19:24 +0100

    update project structure

---
+++ app/utils/qontrol/__init__.py
@@ -0,0 +1,4 @@
+# app/utils\qontrol\__init__.py
+"""
+Qontrol-specific helpers.
+"""
---
+++ app/utils/unitary/__init__.py
@@ -0,0 +1,4 @@
+# app/utils\unitary\__init__.py
+"""
+Modules for unitary transformations and MZI mappings.
+"""
